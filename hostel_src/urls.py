faa4011fc10ab38c1c8decfadfccbc80205d198d
jamesalvine <jamesalvine340@gmail.com>
2023-03-19 16:09:28 +0300

    feature: add custom user model

--- hostel_src/urls.py
+++ hostel_src/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include,path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,3 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("supersecret/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+admin.site.site_header = "Patahostel Admin"
+admin.site.site_title = "Patahostel Admin Portal"
+admin.site.index_title = "Welcome to the Patahostel Portal"
